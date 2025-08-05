e0925ddbc3ce83e969d8672375a81dd9d61efeb7
rutujaa121 <rutujaaugale@gmail.com>
2025-08-04 20:55:51 -0700

    added

--- 03_Assignment/practice/03_learn.php
+++ 03_Assignment/practice/03_learn.php
@@ -8,12 +8,23 @@
     // echo $a;
     // echo $b;
 
-    $i = 1;
-    while($i<=10)
-    {
-        echo $i; "";
-        $i++;
-    }
+    // $i = 1;
+    // while($i<=10)
+    // {
+    //     echo $i; "";
+    //     $i++;
+    // }
 
+
+    $var1 = "php";
+    $var2 = "Java";
+    if($var1 == $var2)
+    {
+        echo "string is equal";
+    }
+    else
+    {
+        echo "string is not equal";
+    }
  ?>
 
