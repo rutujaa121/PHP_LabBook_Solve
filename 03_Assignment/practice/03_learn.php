7373ae973d268ec2cd7958ee63d8492b41e06b63
rutujaa121 <rutujaaugale@gmail.com>
2025-08-04 20:51:53 -0700

    added

--- 03_Assignment/practice/03_learn.php
+++ 03_Assignment/practice/03_learn.php
@@ -3,9 +3,17 @@
     // $$a="php";
     // echo $hello;
     // $hello="php"
-    $a = 2;
-    $b = & $a;
-    echo $a;
-    echo $b;
+    // $a = 2;
+    // $b = & $a;
+    // echo $a;
+    // echo $b;
+
+    $i = 1;
+    while($i<=10)
+    {
+        echo $i; "";
+        $i++;
+    }
+
  ?>
 
