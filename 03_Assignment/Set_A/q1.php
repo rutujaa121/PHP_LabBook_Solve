3ad639d72ff7f3f966f2bcc6ee017be518b310b9
rutujaa121 <rutujaaugale@gmail.com>
2025-08-20 08:22:37 -0700

    added

---
+++ 03_Assignment/Set_A/q1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Integers</legend>
+        <form>
+            <input type="number" name="num1" placeholder="Enter first integer">
+            <input type="number" name="num2" placeholder="Enter second integer">
+
+            1. Mod
+            2. Power 
+            3. Sum 
+            4. Fact
+
+            <input type="number" name="num3" placeholder="Choose a Number" required>
+        </form>
+    </fieldset>
+</body>
+</html>
