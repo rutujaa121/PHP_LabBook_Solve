d8cab4523cf606e3c3b92518635ebf3f2fb87c10
rutujaa121 <rutujaaugale@gmail.com>
2025-08-28 19:36:54 -0700

    added php code

--- 03_Assignment/Set_A/q1.php
+++ 03_Assignment/Set_A/q1.php
@@ -8,18 +8,76 @@
 <body>
     <fieldset>
         <legend>Integers</legend>
-        <form>
-            <input type="number" name="num1" placeholder="Enter first integer">
-            <input type="number" name="num2" placeholder="Enter second integer">
+        <form action="q1a.php" method="POST">
+            <input type="number" name="t1" placeholder="Enter first integer">
+            <input type="number" name="t2" placeholder="Enter second integer">
 
             1. Mod
             2. Power 
             3. Sum 
             4. Fact
 
-            <input type="number" name="num3" placeholder="Choose a Number" required>
+            <input type="number" name="t3" placeholder="Choose a Number" required>
         </form>
     </fieldset>
 </body>
 </html>
 
+<?php
+    $a = $_POST ['t1'];
+    $b = $_POST ['t2'];
+    $ch = $_POST ['t3'];
+
+    switch($ch)
+    {
+        case 1:
+            mod($a, $b);
+            break;
+        case 2:
+            power($a, $b);
+            break;
+        case 3:
+            sum($a);
+            break;
+        case 4:
+            fact($a);
+            break;
+    }
+    function mod($a, $b)
+    {
+        $c = $a % $b;
+        echo "Mod is : ".$c;
+    }
+
+    function power($a, $b)
+    {
+        $p = 1;
+        while($b > 0)
+        {
+            $p = $p * $a;
+            $b--;
+        }
+        echo "Power is : ".$p;
+    }
+
+    function sum ($a)
+    {
+        $sum = 0;
+        while($a > 0)
+        {
+            $sum = $sum + $a;
+            $a--;
+        }
+        echo "Sum is : ".$sum;
+    }
+
+    function fact ($a){
+        $fact = 1;
+        while($a > 0)
+        {
+            $fact = $fact * $a;
+            $a--;
+        }
+        echo "Factorial is : ".$fact;
+    }
+?>
