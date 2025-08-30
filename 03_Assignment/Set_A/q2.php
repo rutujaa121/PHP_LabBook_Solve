c4628c746131968119cb6de512677eb7333f48ef
rutujaa121 <rutujaaugale@gmail.com>
2025-08-29 19:56:07 -0700

    added

--- 03_Assignment/Set_A/q2.php
+++ 03_Assignment/Set_A/q2.php
@@ -7,7 +7,12 @@
 </head>
 <body>
     <form action="q2.php" method="POST">
-        
+        Enter String : <input type="text" name="str"> <br>
+        <input type="radio" name="r" value="1"> Length of String <br>
+        <input type="radio" name="r" value="2"> Length of String <br>
+        <input type="radio" name="r" value="3"> Length of String <br>
+        <input type="radio" name="r" value="4"> Length of String <br>
+
     </form>
 </body>
 </html>
