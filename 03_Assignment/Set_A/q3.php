e0af85f8f543806c389db23d3e132e44a7f546f5
rutujaa121 <rutujaaugale@gmail.com>
2025-09-01 22:17:41 -0700

    added

--- 03_Assignment/Set_A/q3.php
+++ 03_Assignment/Set_A/q3.php
@@ -1,5 +1,5 @@
 <?php 
-// Write a PHP script to check whether a number is even or odd.
+
 $number = 7; // You can change this number to test with different values
 if ($number % 2 == 0) {
     echo "$number is an even number.";
