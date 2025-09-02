77efda3aeb31480da095a7ac493de95c8dc01449
rutujaa121 <rutujaaugale@gmail.com>
2025-09-01 22:15:25 -0700

    added

--- 03_Assignment/Set_A/q3.php
+++ 03_Assignment/Set_A/q3.php
@@ -4,3 +4,6 @@ $number = 7; // You can change this number to test with different values
 if ($number % 2 == 0) {
     echo "$number is an even number.";
 } 
+else {
+    echo "$number is an odd number.";
+}
