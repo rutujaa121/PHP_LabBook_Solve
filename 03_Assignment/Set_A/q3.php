e9ed77e10efcda8a2fb8c3e6c287ab8e3229c954
rutujaa121 <rutujaaugale@gmail.com>
2025-09-02 18:38:59 -0700

    accept two string

--- 03_Assignment/Set_A/q3.php
+++ 03_Assignment/Set_A/q3.php
@@ -1,8 +1,3 @@
 <?php 
-$number = 7; // You can change this number to test with different values
-if ($number % 2 == 0) {
-    echo "$number is an even number.";
-} 
-else {
-    echo "$number is an odd number.";
-}
+
+?>
