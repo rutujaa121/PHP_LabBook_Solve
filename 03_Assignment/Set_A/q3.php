f3dd5c42c95bf861f1ffd58a7ddfef676c7f8e14
rutujaa121 <rutujaaugale@gmail.com>
2025-09-02 18:51:44 -0700

    accept two string

--- 03_Assignment/Set_A/q3.php
+++ 03_Assignment/Set_A/q3.php
@@ -7,8 +7,14 @@
 </head>
 <body>
     <form action="q3.php" method="POST">
-        Enter First String: <input type="text" name="str1"> <br><br>
-        Enter Second String: <input type="text" name="str2"> <br><br>
+        Enter First String: <input type="text" name="str1">
+        Enter Second String: <input type="text" name="str2"> <br><br><br>
+
+        <input type="radio" name="r" value="1"> Find the small string Appears at start of the large string <br>
+        <input type="radio" name="r" value="2"> Find the position of the small string in the big string <br>
+        <input type="radio" name="r" value="3"> Compare both the string for 1st n char. also comparison not be case <br>
+        
+        <input type="submit" value="Submit"> 
     </form>
 </body>
 </html>
