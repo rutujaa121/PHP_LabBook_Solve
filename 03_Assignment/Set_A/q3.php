f1b165f30440655b38e48c07a43b47cd84c5bfcd
rutujaa121 <rutujaaugale@gmail.com>
2025-09-02 18:41:23 -0700

    accept two string

--- 03_Assignment/Set_A/q3.php
+++ 03_Assignment/Set_A/q3.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="q3.php" method="POST">
+        Enter First String: <input type="text" name="str1"> <br><br>
+        Enter Second String: <input type="text" name="str2"> <br><br>
+    </form>
+</body>
+</html>
+
 <?php 
 
 ?>
