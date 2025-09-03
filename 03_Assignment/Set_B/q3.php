7fa27881bf3ebfdbd78b7d0b8946c27bfdc5b394
rutujaa121 <rutujaaugale@gmail.com>
2025-09-03 07:56:37 -0700

    create html code

--- 03_Assignment/Set_B/q3.php
+++ 03_Assignment/Set_B/q3.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="q3.php" method="POST">
+        Enter the name of the Student: <input type="text" name="t1"> 
+        Enter the name of the College: <input type="text" name="t2">
+        Enter your Greeting Message: <input type="text" name="t3">
+
+        <input type="submit" value="Submit">>
+    </form>
 </body>
 </html>
+
