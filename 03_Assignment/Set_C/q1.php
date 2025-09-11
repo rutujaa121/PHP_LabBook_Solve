9c94a36cabf4d3fb1d36fc12436a11e1fc23aab5
rutujaa121 <rutujaaugale@gmail.com>
2025-09-10 23:09:58 -0700

    added name

--- 03_Assignment/Set_C/q1.php
+++ 03_Assignment/Set_C/q1.php
@@ -8,5 +8,6 @@
 <body>
     <form action="q1.php" method="POST">
         Enter roll number: <input type="text" name="r"> <br><br>
+        Enter name: <input type="text" name="n"> <br><br>
 </body>
 </html>
