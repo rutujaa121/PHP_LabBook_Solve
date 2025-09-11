801ee045d6b4ec81c58fbe04fe2d3ebb40169466
rutujaa121 <rutujaaugale@gmail.com>
2025-09-10 23:09:23 -0700

    added roll no

---
+++ 03_Assignment/Set_C/q1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="q1.php" method="POST">
+        Enter roll number: <input type="text" name="r"> <br><br>
+</body>
+</html>
