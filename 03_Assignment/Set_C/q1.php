12eb6855d91ece3c9b29e135b9ccb2f932098c08
rutujaa121 <rutujaaugale@gmail.com>
2025-09-16 22:01:05 -0700

    add table

--- 03_Assignment/Set_C/q1.php
+++ 03_Assignment/Set_C/q1.php
@@ -32,5 +32,41 @@
         $s4 = explode(",", $_POST['s4']);
         $s5 = explode(",", $_POST['s5']);
     ?>
+    <center>
+        <table border="1">
+            <?php
+                $c = count($r);
+                echo "<tr>
+                        <th>Roll No. </th>
+                        <th>Name</th>
+                        <th>Java</th>
+                        <th>PHP</th>
+                        <th>OS</th>
+                        <th>TCS</th>
+                        <th>Stat</th>
+                        <th>Total</th>
+                        <th>Percentage</th>
+                    </tr>";
+                        for($i=0; $i<$c; $i++)
+                        {
+                            $t = $s1[$i] + $s2[$i] + $s3[$i] + $s4[$i] + $s5[$i];
+                            $p = $t/5;
+
+                            echo "<tr>
+                                    <td>".$r[$i]."</td>
+                                    <td>".$n[$i]."</td>
+                                    <td>".$s1[$i]."</td>
+                                    <td>".$s2[$i]."</td>
+                                    <td>".$s3[$i]."</td>
+                                    <td>".$s4[$i]."</td>
+                                    <td>".$s5[$i]."</td>
+                                    <td>".$t."</td>
+                                    <td>".$p."%</td>
+                                </tr>";
+                        }
+                        
+            ?>
+        </table>
+    </center>
 </body>
 </html>
