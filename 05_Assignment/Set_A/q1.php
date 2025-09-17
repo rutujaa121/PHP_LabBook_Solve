5daa9b7f95ae41f614cf30967a540e9a447bc26e
rutujaa121 <rutujaaugale@gmail.com>
2025-09-16 22:12:35 -0700

    add files type in html

---
+++ 05_Assignment/Set_A/q1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="q1.php" method="post">
+        Enter the name: <input type="text" name="n"> <br><br>
+        1. Display the type of file.
+        2. Display last modified time of file.
+        3. Display the size of file.
+        4. Delete the file. 
+    </form>
+</body>
+</html>
