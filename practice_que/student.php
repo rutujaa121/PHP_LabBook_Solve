c9f7c510501a857419c15dd52677011509f2bcb3
rutujaa121 <rutujaaugale@gmail.com>
2025-09-23 20:23:42 -0700

    added

---
+++ practice_que/student.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+        <?php
+            $f = fopen("student.dat", "r");
+
+            echo "<tr>
+                <th>Roll No </th>
+                <th>Name </th>
+                <th>OS</th>
+                <th>WT</th>
+                <th>Python</th>
+                <th>Java</th>
+                <th>CN</th>
+                <th>DS</th>
+                <th>Total</th>
+                <th>Percentage</th>
+               </tr>";
+            
+            while(($line = fgets($f))!== false)
+            {
+                $data = explode(",",trim );
+            }
+        ?>
+    </table>
+</body>
+</html>
